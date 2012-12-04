f1f2ddb868ffe833f0bcd6d6dcbe76fc32cbde2e
David Baines <dave.baines@me.com>
2012-12-04 15:09:39 -0500

    Fixed login jquery issues

--- virska.com/js/login.js
+++ virska.com/js/login.js
@@ -2,14 +2,13 @@
 
 $(document).ready(function() {
 	
-		$('#loginForm').submit(function() {
-			if ($("#loginFormEmailInput").length == 0 && $("#loginFormPasswordInput").length == 0) {
-				$("#noValuesError").show();	
-				$('#loginForm').effect("shake", { times:2 }, 50);
-				return false;		
-		    } else {
-				return true;
-			}
-	    });
+    $("form").submit(function() {
+      if ($("input").val() != 0) {
+        return true;
+      }
+      	$("#noValuesError").show();
+		$('#loginForm').effect("shake", { times:2 }, 50);
+      return false;
+    });
 		
 });
